76e131b288e929a9cf70612381a0d44ab300052e
Rudolf Zanoni <rudolf.zanoni@devplace.com.br>
2018-12-14 14:02:43 -0200

    [TEC] Validação do Sistema;

--- WebService/Models/Job/VM/JobsRegistrationVM.cs
+++ WebService/Models/Job/VM/JobsRegistrationVM.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace WebService
 {
@@ -7,9 +6,13 @@ namespace WebService
     {
         public int jobId { get; set; }
 
-        public string code { get; set; }
-
         public string registration { get; set; }
+
+        public string name { get; set; }
+
+        public string course { get; set; }
+
+        public string unity { get; set; }
 
 
         public List<JobCategoriesRegistrationVM> jobCategories { get; set; }
