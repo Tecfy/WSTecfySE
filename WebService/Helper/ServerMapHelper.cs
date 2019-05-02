0d70d234c42e2bb31baa5dd0290f981dd49de7bb
Rudolf Zanoni <rudolf@devplace.com.br>
2019-05-02 18:10:21 -0300

    [TEC] Lentidão na exibição das imagens;

--- WebService/Helper/ServerMapHelper.cs
+++ WebService/Helper/ServerMapHelper.cs
@@ -1,4 +1,4 @@
-﻿using System.Web;
+﻿using System.Web.Configuration;
 
 namespace WebService.Helper
 {
@@ -6,11 +6,14 @@ namespace WebService.Helper
     {
         public static string GetServerMap(string appPath)
         {
-            string location = HttpContext.Current.Server.MapPath("~");
+            //string location = HttpContext.Current.Server.MapPath("~");
 
-            location = location.Substring(0, location.Length - 1);
-            location = location.Substring(0, location.LastIndexOf("\\"));
-            location = location + appPath;
+            //location = location.Substring(0, location.Length - 1);
+            //location = location.Substring(0, location.LastIndexOf("\\"));
+
+            string location = WebConfigurationManager.AppSettings["Path"];
+
+            location = location + appPath;            
 
             return location;
         }
